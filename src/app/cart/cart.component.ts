7d135f56395a3fcf50abf55094032c22fe98b968
Johannes Zelenak <jf.zelenak@gmail.com>
2023-10-20 15:07:41 +0200

    add changes to cart comp

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IDishes } from '../IDishes';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  items: IDishes[] = [];
 
+  constructor(private CS: CartService) {}
+
+  ngOnInit(): void {
+    this.items = this.CS.getItems();  
+  }
 }
