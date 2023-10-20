243c6f616e46a78094e3b7529861ad97a03ba657
Johannes Zelenak <jf.zelenak@gmail.com>
2023-10-20 13:42:01 +0200

    add layout of dishes pages

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IDishes } from '../IDishes';
+import { ActivatedRoute, Params } from '@angular/router';
+import { dishes } from '../dishes';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
 
+  dish: IDishes = {} as IDishes;
+  id: number = 0;
+
+  constructor(private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.dish = dishes[this.id];
+    })
+  }
 }
