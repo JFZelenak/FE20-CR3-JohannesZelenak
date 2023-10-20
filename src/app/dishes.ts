10e130e4ce988815e50fd83ce19323cf810c42de
Johannes Zelenak <jf.zelenak@gmail.com>
2023-10-20 22:40:17 +0200

    change price values

--- src/app/dishes.ts
+++ src/app/dishes.ts
@@ -3,63 +3,63 @@ export const dishes = [
         name: "Pasta Gambero Rosso",
         image: "./assets/images/dishes/pasta-gambero-rosso3.jpg",
         description: "our finest homemade pasta with red shrimps and our homemade tomato sauce",
-        price: 10,
+        price: 19.90,
         available: true,
         vegetarian: false
     },{
         name: "Pasta Pomodoro",
         image: "./assets/images/dishes/pasta-pomodoro.jpg",
         // description: "our finest homemade pasta with our homemade tomato sauce",
-        price: 10,
+        price: 11.90,
         available: true,
         vegetarian: true  
     },{
         name: "Pasta Spinaci",
         image: "./assets/images/dishes/pasta-spinaci.jpg",
         description: "our finest homemade pasta with a fine homemade spinach pesto with spinach from our garden",
-        price: 10,
+        price: 14.50,
         available: false,
         vegetarian: true  
     },{
         name: "Pasta Vegetariana",
         image: "./assets/images/dishes/pasta-vegetariana.jpg",
         description: "our finest homemade pasta with beans, tomatoes and our homemade tomato sauce",
-        price: 10,
+        price: 15.75,
         available: true,
         vegetarian: true  
     },{
         name: "Pizza Frutti Di Mare",
         image: "./assets/images/dishes/pizza-frutti-di-mare.jpg",
         description: "our famouse fresh made pizza with local seafood on top",
-        price: 10,
+        price: 18.50,
         available: true,
         vegetarian: false 
     },{
         name: "Pizza Polpette",
         image: "./assets/images/dishes/pizza-polpette.jpg",
         description: "our famouse fresh made pizza with homemade meatballs and lots of cheese",
-        price: 10,
+        price: 17.90,
         available: true,
         vegetarian: false 
     },{
         name: "Pizza Provinciale",
         image: "./assets/images/dishes/pizza-provinciale.jpg",
         description: "our famouse fresh made pizza with bacon, mushrooms and tomatoes",
-        price: 10,
+        price: 17.30,
         available: true,
         vegetarian: false  
     },{
         name: "Pizza Speciale",
         image: "./assets/images/dishes/pizza-speciale.jpg",
         description: "our famouse fresh made pizza with spinach, pine nuts, olives and anchovies",
-        price: 10,
+        price: 16.90,
         available: true,
         vegetarian: false  
     },{
         name: "Pizza vegetariana",
         image: "./assets/images/dishes/pizza-vegetariana.jpg",
         description: "our famouse fresh made pizza with the best veggies from our garden and from local farms",
-        price: 10,
+        price: 15.10,
         available: true,
         vegetarian: true  
     }
