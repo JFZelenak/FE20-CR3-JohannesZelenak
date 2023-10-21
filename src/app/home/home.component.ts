2db58dd77400c62a0a1b002fdae6c8181d42d07e
Johannes Zelenak <jf.zelenak@gmail.com>
2023-10-21 11:32:08 +0200

    add home page stuff

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IDishes } from '../IDishes';
+import { dishes } from '../dishes';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  
+  dishes: IDishes[] = dishes;
 
+  ngOnInit(): void {
+  }
 }
